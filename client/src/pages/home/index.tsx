d6131df643c03b1087ef584b37ee5ce20890be1c
mudassar755 <mudasar.se@gmail.com>
2021-05-10 13:32:02 +0500

    heading updated

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -11,7 +11,7 @@ const Home = () => {
     return (
         <div className="carousel-image-1">
             <div className="content text-center text-white">
-                <h1 className="mb-3">Hello World</h1>
+                <h1 className="mb-3">MERN Stack Authentication</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident nostrum quo atque? Sequi adipisci inventore molestias! Veritatis in ratione, aliquam cum officiis vero quaerat repudiandae porro exercitationem nemo reiciendis quam recusandae perspiciatis eos, quae architecto sed. Repellat dolore optio nemo, harum temporibus cumque molestias fugiat? Facilis quos fugiat suscipit iste.</p>
 
             </div>
